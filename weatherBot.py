216de72a12aeb012940837eec1dbc26c78a9afd4
vlad1688 <Vlad.Imanaeiv@mail.ru>
2023-06-14 14:00:37 +0500

    WeatherBot
    WeatherBot

--- weatherBot.py
+++ weatherBot.py
@@ -5,6 +5,7 @@ import requests
 bot = telebot.TeleBot("6121313023:AAFTJhDHwRygi5tRFC49zIDAIGJOLedBHXc")
 API = "29d4796418a2468831b621594537b4b4"
 
+
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, "Привет, напиши название города")
@@ -16,7 +17,15 @@ def weather(message):
     res = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric")
     if res.status_code == 200:
         weatherCity = json.loads(res.text)
-        bot.reply_to(message, f'Сейчас погода: {weatherCity["main"]["temp"]}')
+        temp = weatherCity["main"]["temp"]
+        feels_like_temp = weatherCity["main"]["feels_like"]
+        wind = weatherCity["wind"]["speed"]
+        bot.reply_to(message, f'Сейчас погода: {temp}\nЧуствуеться как: {feels_like_temp}\nВетер: {wind} м/c')
+
+        image = "sunny.png" if temp > 15.0 else 'cloudy.png'
+        file = open('./' + image, 'rb')
+        bot.send_photo(message.chat.id, file)
     else:
         bot.reply_to(message, "Город введен не верно")
+
 bot.polling(none_stop=True)
